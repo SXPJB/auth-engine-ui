ec2a204cd4bef3a549bf848ce7ac3f8286bf6b55
sxpjb <emmanuel.hernandezramirez@gmail.com>
2024-03-19 09:40:00 -0600

    [SXPJB] integrate eslint and prettier also added the lint workflow

--- src/app/auth-container/auth-container.component.ts
+++ src/app/auth-container/auth-container.component.ts
@@ -1,13 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth-container',
   standalone: true,
   imports: [],
   templateUrl: './auth-container.component.html',
-  styleUrl: './auth-container.component.css'
+  styleUrl: './auth-container.component.css',
 })
-export class AuthContainerComponent {
-
-
-}
+export class AuthContainerComponent {}
--- src/app/registration/registration.component.spec.ts
+++ src/app/registration/registration.component.spec.ts
@@ -1,32 +1,33 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { type ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {RegistrationComponent} from './registration.component';
-import {ActivatedRoute} from "@angular/router";
-import {of} from "rxjs";
-import {HttpClientModule} from "@angular/common/http";
+import { RegistrationComponent } from './registration.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('RegistrationComponent', () => {
-    let component: RegistrationComponent;
-    let fixture: ComponentFixture<RegistrationComponent>;
+  let component: RegistrationComponent;
+  let fixture: ComponentFixture<RegistrationComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [RegistrationComponent, HttpClientModule],
-            providers: [{
-                provide: ActivatedRoute,
-                useValue: {
-                    paramMap: of()
-                }
-            }]
-        })
-            .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegistrationComponent, HttpClientModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(),
+          },
+        },
+      ],
+    }).compileComponents();
 
-        fixture = TestBed.createComponent(RegistrationComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    fixture = TestBed.createComponent(RegistrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
